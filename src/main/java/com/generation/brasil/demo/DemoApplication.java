fba4fe82756ba26cf0f66c4dfda1f7dc8e8b42d2
Gian de Manincor <gianmanincor@gmail.com>
2022-09-30 17:29:50 -0300

    feat: config open api

--- src/main/java/com/generation/brasil/demo/DemoApplication.java
+++ src/main/java/com/generation/brasil/demo/DemoApplication.java
@@ -1,14 +1,28 @@
 package com.generation.brasil.demo;
 
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("Demo API")
+				.description("Spring demo api")
+				.version("v1")
+				.description("SpringShop Wiki Documentation"));
 	}
 
 }
